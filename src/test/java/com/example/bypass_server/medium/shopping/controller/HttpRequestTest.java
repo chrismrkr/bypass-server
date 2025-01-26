ab43f050f643a4a040f316c3482afde067943944
chrismrkr <b635032_@daum.net>
2025-01-27 01:40:15 +0900

    edit

--- src/test/java/com/example/bypass_server/medium/shopping/controller/HttpRequestTest.java
+++ src/test/java/com/example/bypass_server/medium/shopping/controller/HttpRequestTest.java
@@ -213,4 +213,58 @@ public class HttpRequestTest {
         Assertions.assertEquals(10000-100*10, itemEntity.getStock());
         Assertions.assertEquals(10000-100*10, shoppingPointEntity.getPoint());
     }
+
+    @Test
+    void 아이템구매_요청_동시_10명이_2번씩() throws Exception {
+        // given
+        ItemEntity savedItem = itemJpaRepository.save(
+                ItemEntity.builder()
+                        .itemName("TestItem")
+                        .price(1)
+                        .stock(100000)
+                        .build()
+        );
+        for(int i=0; i<10; i++) {
+            ShoppingPointEntity savedShoppingPoints = shoppingPointJpaRepository.save(
+                    ShoppingPointEntity.builder()
+                            .memberId((long)i)
+                            .point(10000L)
+                            .build()
+            );
+        }
+        // when
+        int threadCount = 20;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+        for(int i=0; i<threadCount; i++) {
+            int finalI = i;
+            executorService.execute(() -> {
+                int buyAmt = 100;
+                ShoppingRequestDto reqDto = ShoppingRequestDto
+                        .builder()
+                        .memberId((long)(finalI)%10)
+                        .itemId(savedItem.getId()).amount(buyAmt).build();
+                try {
+                    MvcResult mvcResult = mockMvc.perform(patch("/item")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(reqDto)))
+                            .andExpect(status().isOk())
+                            .andReturn();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        // then
+        Thread.sleep(1000L);
+        ItemEntity itemEntity = itemJpaRepository.findById(savedItem.getId()).get();
+        Assertions.assertEquals(100000-100*10*2, itemEntity.getStock());
+        for(int i=0; i<10; i++) {
+            ShoppingPointEntity shoppingPointEntity = shoppingPointJpaRepository.findById((long) i).get();
+            Assertions.assertEquals(10000L - 100*2, shoppingPointEntity.getPoint());
+        }
+    }
 }
